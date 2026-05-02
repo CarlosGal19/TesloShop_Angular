1d7a1928bf446d4d15b46afc8c864b1f5feac959
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-02 16:20:31 -0600

    feat: show nav bar in layout

--- src/app/store/layouts/store-layout/store-layout.ts
+++ src/app/store/layouts/store-layout/store-layout.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderNavbar } from '../../components/header-navbar/header-navbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'layout-store-layout',
-  imports: [],
+  imports: [
+    HeaderNavbar,
+    RouterOutlet
+],
   templateUrl: './store-layout.html',
 })
 export class StoreLayout {
