68979518e1ac385c32e2241b9d76cd2e085d2a81
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-03 13:56:54 -0600

    feat: render products by gender

--- src/app/store/pages/gender-page/gender-page.ts
+++ src/app/store/pages/gender-page/gender-page.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { ProductsService } from '@products/services/products.service';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { map, of } from 'rxjs';
+import { ProductCard } from "@products/components/product-card/product-card";
 
 @Component({
   selector: 'gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.html',
 })
 export class GenderPage {
+  productsService = inject(ProductsService);
+  route = inject(ActivatedRoute);
 
+  gender = toSignal(
+    this.route.params.pipe(
+      map(({ gender }) => gender),
+    )
+  )
+
+  genderProductsResource = rxResource({
+    params: () => ({
+      gender: this.gender()
+    }),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({ gender: params.gender })
+    }
+  })
 }
