9b07b290de67cbf6b32d73c22696a6e7ad8c1b8e
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-03 10:44:07 -0600

    feat: render fetched products

--- src/app/store/pages/home-page/home-page.ts
+++ src/app/store/pages/home-page/home-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { ProductsService } from '@products/services/products.service';
+import { Component, inject } from '@angular/core';
 import { ProductCard } from '@products/components/product-card/product-card';
+import { rxResource } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'home-page',
@@ -9,5 +11,14 @@ import { ProductCard } from '@products/components/product-card/product-card';
   templateUrl: './home-page.html',
 })
 export class HomePage {
+  private productsService = inject(ProductsService);
 
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({
+        limit: 12
+      });
+    },
+  });
 }
