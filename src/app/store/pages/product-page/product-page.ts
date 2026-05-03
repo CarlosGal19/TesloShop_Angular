a2a270187b98115226c3edcf3e7a60ce309a6a78
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-03 13:12:13 -0600

    feat: render product data

--- src/app/store/pages/product-page/product-page.ts
+++ src/app/store/pages/product-page/product-page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
 
 @Component({
   selector: 'product-page',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './product-page.html',
 })
 export class ProductPage {
+  productsService = inject(ProductsService);
+  activatedRoute = inject(ActivatedRoute);
 
+  queryParam = this.activatedRoute.snapshot.paramMap.get('id') ?? ''
+
+  term = signal(this.queryParam);
+
+  productResource = rxResource({
+    params: () => ({
+      term: this.term()
+    }),
+    stream: ({ params }) => {
+      return this.productsService.getProductByTerm(params.term)
+    }
+  })
 }
