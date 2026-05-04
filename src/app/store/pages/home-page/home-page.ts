42cc036258d84357ac5711b5a17a69fca2799988
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-03 20:57:24 -0600

    feat: show pagination in home page

--- src/app/store/pages/home-page/home-page.ts
+++ src/app/store/pages/home-page/home-page.ts
@@ -2,22 +2,29 @@ import { ProductsService } from '@products/services/products.service';
 import { Component, inject } from '@angular/core';
 import { ProductCard } from '@products/components/product-card/product-card';
 import { rxResource } from '@angular/core/rxjs-interop'
+import { Pagination } from "@shared/components/pagination/pagination";
+import { PaginationService } from '@shared/components/services/pagination';
 
 @Component({
   selector: 'home-page',
   imports: [
-    ProductCard
-  ],
+    ProductCard,
+    Pagination
+],
   templateUrl: './home-page.html',
 })
 export class HomePage {
   private productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
   productsResource = rxResource({
-    params: () => ({}),
+    params: () => ({
+      page: this.paginationService.currentPage()
+    }),
     stream: ({ params }) => {
       return this.productsService.getProducts({
-        limit: 12
+        limit: 12,
+        offset: (params.page - 1) * 12
       });
     },
   });
