61f3a588eda89c8ac26089ade77e3e2b2808084d
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-05 18:55:57 -0600

    feat: show error message if data is invalid

--- src/app/auth/pages/login/login.ts
+++ src/app/auth/pages/login/login.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms'
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
 })
 export class Login {
+  formBuilder = inject(FormBuilder);
+  hasError = signal<boolean>(false);
+  isPosting = signal<boolean>(false);
 
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+      return;
+    }
+    console.log(this.loginForm.value)
+  }
 }
