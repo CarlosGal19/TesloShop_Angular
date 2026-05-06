181544dc4329e1890fd6e872581a97b365b452a2
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-05 19:45:23 -0600

    feat: check status based on local storage saved token

--- src/app/auth/services/auth-service.ts
+++ src/app/auth/services/auth-service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { IAuthResponse } from '@auth/interfaces/auth-response.interface';
 import { IUser } from '@auth/interfaces/user.interface';
 import { catchError, map, Observable, of, tap } from 'rxjs';
@@ -29,6 +30,10 @@ export class AuthService {
 
   http = inject(HttpClient);
 
+  validateStatusResource = rxResource({
+    stream: () => this.checkStatus()
+  })
+
   login(email: string, password: string): Observable<boolean> {
     return this.http.post<IAuthResponse>(`${baseUrl}/auth/login`, {
       email,
@@ -53,4 +58,37 @@ export class AuthService {
       })
     )
   };
+
+  checkStatus(): Observable<boolean> {
+    const authToken = localStorage.getItem('authToken') as string;
+    if (!authToken) {
+      return of(false);
+    }
+
+    return this.http.get<IAuthResponse>(`${baseUrl}/auth/check-status`, {
+      headers: {
+        Authorization: `Bearer ${authToken}`
+      }
+    }).pipe(
+      tap(response => {
+        this._authStatus.set('auth');
+        this._user.set(response.user);
+        this._token.set(response.token);
+
+        localStorage.setItem('authToken', response.token);
+      }),
+      map(() => true),
+      catchError((error: any) => {
+        this._authStatus.set('not-auth');
+        this._user.set(null);
+        this._token.set(null);
+
+        localStorage.removeItem('authToken');
+
+        return of(false);
+      })
+    )
+
+    return of(true);
+  }
 }
