f5d57b7460e423e047e1d5feabb23a3cf67fee71
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-06 20:50:54 -0600

    feat: implement auth guard in auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '@auth/guards/auth-guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.routes')
+    loadChildren: () => import('./auth/auth.routes'),
+    canMatch: [authGuard]
   },
   {
     path: '',
