e24c49bed490afa7590626e9f9957d32b882b4f1
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-09 14:10:49 -0600

    feat: create page for viewing product data

---
+++ src/app/admin/pages/product-admin/product-details/product-details.ts
@@ -0,0 +1,52 @@
+import { Component, inject, input, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProductCarousel } from "@products/components/product-carousel/product-carousel";
+import { IProduct } from '@products/interfaces/product-response.interface';
+import { ProductsService } from '@products/services/products.service';
+import { FormUtils } from '@utils/form-utils';
+
+@Component({
+  selector: 'product-details',
+  imports: [ProductCarousel, ReactiveFormsModule],
+  templateUrl: './product-details.html',
+})
+export class ProductDetails implements OnInit {
+  formBuilder = inject(FormBuilder);
+  productsService = inject(ProductsService);
+
+  product = input.required<IProduct>();
+
+  sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  productForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      slug: [
+        '',
+        [Validators.required, Validators.pattern(FormUtils.slugPattern)],
+      ],
+      price: [0, [Validators.required, Validators.min(0)]],
+      stock: [0, [Validators.required, Validators.min(0)]],
+      sizes: [['']],
+      images: [[]],
+      tags: [''],
+      gender: [
+        'men',
+        [Validators.required, Validators.pattern(/men|women|kid|unisex/)],
+      ],
+    });
+
+  ngOnInit() {
+    this.setFormData(this.product());
+  }
+
+  setFormData(formLike: Partial<IProduct>) {
+    this.productForm.reset(formLike as any)
+    // this.productForm.patchValue(formLike as any);
+    this.productForm.patchValue({ 'tags': formLike.tags?.join('') });
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value)
+  }
+}
