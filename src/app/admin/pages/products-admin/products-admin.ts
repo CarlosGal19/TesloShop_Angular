5eee3925109ef60af055a85d7e8756697a498832
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-09 10:13:00 -0600

    feat: fetch and render products with pagination

--- src/app/admin/pages/products-admin/products-admin.ts
+++ src/app/admin/pages/products-admin/products-admin.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Pagination } from "@shared/components/pagination/pagination";
+import { ProductsTable } from "@products/components/products-table/products-table";
+import { ProductsService } from '@products/services/products.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { PaginationService } from '@shared/components/services/pagination';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'products-admin',
-  imports: [],
+  imports: [Pagination, ProductsTable],
   templateUrl: './products-admin.html',
 })
 export class ProductsAdmin {
+  productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
+  productsPerPage = signal(10);
+
+  productsResource = rxResource({
+    params: () => ({
+      limit: this.productsPerPage(),
+      page: this.paginationService.currentPage()
+    }),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({
+        limit: params.limit,
+        offset: (params.page - 1) * params.limit
+      }).pipe(
+        tap(data => {
+          console.log(data);
+        })
+      );
+      }
+  })
 }
