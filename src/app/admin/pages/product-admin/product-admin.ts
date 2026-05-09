74d7b6951310da30427d11801fb3ab10cbd1abc3
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-09 13:26:39 -0600

    feat: fetch product data when page is visited

--- src/app/admin/pages/product-admin/product-admin.ts
+++ src/app/admin/pages/product-admin/product-admin.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { ProductDetails } from './product-details/product-details';
 
 @Component({
   selector: 'product-admin',
-  imports: [],
+  imports: [ProductDetails],
   templateUrl: './product-admin.html',
 })
 export class ProductAdmin {
+  productsService = inject(ProductsService);
+  router = inject(Router);
+  activatedRoute = inject(ActivatedRoute);
 
+  id = this.activatedRoute.snapshot.paramMap.get('productId') ?? '';
+
+  term = signal(this.id);
+
+  productResource = rxResource({
+    params: () => ({
+      term: this.term()
+    }),
+    stream: ({ params }) => {
+      return this.productsService.getProductByTerm(params.term);
+    }
+  });
+
+  redirectEffect = effect(() => {
+    if (this.productResource.error()) {
+      this.router.navigate(['/admin/products']);
+    }
+  });
 }
