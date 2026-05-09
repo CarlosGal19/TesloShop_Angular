15384baa4ce2107fd2f75886d79286c11db3f639
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-09 09:32:12 -0600

    feat: create main admin pages

---
+++ src/app/admin/pages/product-admin/product-admin.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-admin',
+  imports: [],
+  templateUrl: './product-admin.html',
+})
+export class ProductAdmin {
+
+}
---
+++ src/app/admin/pages/products-admin/products-admin.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-admin',
+  imports: [],
+  templateUrl: './products-admin.html',
+})
+export class ProductsAdmin {
+
+}
