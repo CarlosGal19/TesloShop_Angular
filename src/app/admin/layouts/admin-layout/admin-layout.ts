a3a64e474a20915e4f38d975333453524416c34e
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-09 09:36:14 -0600

    fix: import required router classes

--- src/app/admin/layouts/admin-layout/admin-layout.ts
+++ src/app/admin/layouts/admin-layout/admin-layout.ts
@@ -1,10 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '@auth/services/auth-service';
 
 @Component({
   selector: 'admin-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './admin-layout.html',
 })
 export class AdminLayout {
