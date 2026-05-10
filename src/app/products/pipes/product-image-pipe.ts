f295b1cc99c028f9e7bd49297d17f8ceea6a33f2
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-10 17:14:25 -0600

    fix: return full url when image is in browser (blob)

--- src/app/products/pipes/product-image-pipe.ts
+++ src/app/products/pipes/product-image-pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { isNotFound } from '@angular/core/primitives/di';
 import { environment } from 'src/environments/environment';
 
 const baseUrl = environment.baseUrl;
@@ -9,13 +10,16 @@ const baseUrl = environment.baseUrl;
 export class ProductImagePipe implements PipeTransform {
 
   transform(value: string | string[]): string {
-
     if ((Array.isArray(value) && value.length === 0)  || !value) {
       return `/assets/images/placeholder.jpg`;
     }
 
     if (Array.isArray(value) && value.length > 0) {
       return `${baseUrl}/files/product/${value[0]}`
+    }
+
+    if (typeof value === 'string' && value.includes('blob')) {
+      return value
     }
 
     return `${baseUrl}/files/product/${value}`
